8434c90a031eec185cb63a67be4e993cbd46fcd3
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2025-02-07 18:41:45 +0000

    basic websocket ping pong

--- Server/main.ts
+++ Server/main.ts
@@ -1,8 +1,21 @@
-export function add(a: number, b: number): number {
-  return a + b;
+function handler(_req: Request): Response {
+  if (_req.headers.get("upgrade") != "websocket") {
+    return new Response(null, { status: 501 });
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(_req);
+
+  socket.addEventListener("open", () => {
+    console.log("a client connected!");
+  });
+
+  socket.addEventListener("message", (event) => {
+    if (event.data === "ping") {
+      socket.send("pong");
+    }
+  });
+
+  return response;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+Deno.serve(handler);
